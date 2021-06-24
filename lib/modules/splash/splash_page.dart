182a7ce940e667cdba0a8d585b3486bc495c6390
felipe.bertanha.pra <felipe.bertanha@capitaletecnologia.com.br>
2021-06-24 12:18:15 -0300

    aula 1

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,24 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        fit: StackFit.expand,
+        alignment: AlignmentDirectional.center,
+        children: [
+          Image.asset(AppImages.union),
+          Image.asset(AppImages.logoFull),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/social_login/social_login_button.dart
@@ -0,0 +1,47 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const SocialLoginButton({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.shape,
+          border: Border.fromBorderSide(BorderSide(color: AppColors.stroke)),
+          borderRadius: BorderRadius.circular(5)
+        ),
+        child: Row(
+          children: [
+            SizedBox(width: 16,),
+            Image.asset(
+              AppImages.google,
+              alignment: AlignmentDirectional.center,
+            ),
+            VerticalDivider(width: 30,thickness: 1, color: AppColors.stroke,),
+            Container(
+              child:
+              Expanded(
+                child: Text(
+                  "Entrar com Google",
+                  style: AppTextStyles.buttonGray,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
